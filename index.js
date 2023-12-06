ec531e3c1f5f491622aa98d18ddc41ee4bb89c97
ginger <26461046+gingerchew@users.noreply.github.com>
2023-12-06 12:27:12 -0500

    Fixes issue with import path when adding toolbar app

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
  * @type {() => import('astro').AstroIntegration}
  */
 
+import path from 'path';
+import url from 'url';
+
 export default () => ({
     name: 'toolbar-confetti',
     hooks: {
         "astro:config:setup": ({ addDevToolbarApp }) => {
-            addDevToolbarApp('./toolbar.js');
+            const importPath = path.dirname(url.fileURLToPath(import.meta.url));
+            const pluginPath = path.join(importPath, 'toolbar.js');
+
+            addDevToolbarApp(pluginPath);
         }
     }
 })
