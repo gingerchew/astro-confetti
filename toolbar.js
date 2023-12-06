5a9c4188e7a66e3075bb3baa6b89ed1d75ad8f2c
ginger <26461046+gingerchew@users.noreply.github.com>
2023-12-06 15:09:16 -0500

    Fixes some argument names

--- toolbar.js
+++ toolbar.js
@@ -4,8 +4,8 @@ export default {
     name: 'Confetti',
     id:'toolbar-confetti',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 16 16"><text x="0" y="14">🎉</text></svg>`,
-    init(canvas, eventTarget) {
-        eventTarget.addEventListener('app-toggled', (e) => {
+    init(_canvas, eventTarget) {
+        eventTarget.addEventListener('app-toggled', () => {
             if (!confetti) {
                 confetti = new JSConfetti()
             }
